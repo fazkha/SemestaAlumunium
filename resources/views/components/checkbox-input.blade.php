70aff5c62fb2d9075d4cd0a271d1623ed0151093
fazkha <mr.eko.handriyanto@gmail.com>
2026-07-15 12:36:00 +0700

    next commit

--- resources/views/components/checkbox-input.blade.php
+++ resources/views/components/checkbox-input.blade.php
@@ -4,4 +4,4 @@
 ])
 
 <input type="checkbox" @disabled($disabled) @required($required) {{ $checked ?? '' }}
-    {{ $attributes->merge(['class' => 'w-6 h-6 block text-md rounded-sm shadow-md text-blue-600 bg-gray-100 border-gray-300 rounded-sm focus:ring-blue-500 focus:ring-2']) }} />
+    {{ $attributes->merge(['class' => 'w-6 h-6 block text-md rounded-sm shadow-md text-blue-600 bg-gray-100 border-gray-300 rounded-sm focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600']) }} />
--- resources/views/components/text-input.blade.php
+++ resources/views/components/text-input.blade.php
@@ -1,3 +1,7 @@
-@props(['disabled' => false])
+@props([
+    'disabled' => false,
+    'required' => false,
+])
 
-<input @disabled($disabled) {{ $attributes->merge(['class' => 'border-gray-300 focus:border-indigo-500 focus:ring-indigo-500 rounded-md shadow-sm']) }}>
+<input @disabled($disabled) @required($required)
+    {{ $attributes->merge(['class' => 'w-full block text-sm rounded-lg shadow-md text-gray-700 placeholder-gray-300 border bg-primary-20 border-primary-100 dark:placeholder-gray-600 dark:border-primary-800 dark:bg-primary-700 dark:text-gray-300 disabled:bg-primary-50 disabled:dark:bg-primary-800 disabled:text-gray-900 disabled:border-primary-100 disabled:dark:border-primary-800']) }} />
