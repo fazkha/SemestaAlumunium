36107dd19610be2223786f49a9e7dcdf429c9156
fazkha <mr.eko.handriyanto@gmail.com>
2026-07-20 11:26:22 +0700

    next commit

--- resources/views/satuan/delete.blade.php
+++ resources/views/satuan/delete.blade.php
@@ -7,15 +7,10 @@
     <div class="flex items-center justify-between px-4 py-4 border-b border-primary-100 lg:py-6">
         <h1 class="text-xl flex items-center justify-center">
             <a href="{{ route('units.index') }}" class="flex items-center justify-center">
-                <svg fill="currentColor" class="w-7 h-7" viewBox="-0.77 0 50 50" xmlns="http://www.w3.org/2000/svg">
-                    <g id="_3" data-name="3" transform="translate(-290.767 -130.5)">
-                        <path id="Path_224" data-name="Path 224"
-                            d="M337.753,130.5h-45.5c-.818,0-1.483.485-1.483,1.081a1.148,1.148,0,0,0,.943,1h46.584a1.147,1.147,0,0,0,.939-1C339.233,130.985,338.57,130.5,337.753,130.5Z" />
-                        <path id="Path_225" data-name="Path 225"
-                            d="M335.961,177.3h-.439V162.5a20.258,20.258,0,0,0,.013-3.459,20.081,20.081,0,0,0-16.913-19.822v-2.266h15.692a1.406,1.406,0,0,0,1.446-1.364l.947-1.954H293.294l.889,1.954a1.407,1.407,0,0,0,1.448,1.364h16.3v2.32c-.092.016-.184.029-.275.046a20.087,20.087,0,0,0-16.26,18.789,1.674,1.674,0,0,0-.137.656V177.3h-1.215a1.6,1.6,0,0,0,0,3.2h41.92a1.6,1.6,0,0,0,0-3.2Zm-20.018-5.067v-2.577h-.732v2.577a13.6,13.6,0,0,1-13.163-13.662c0-.043.005-.084.005-.126h2.381v-.73H302.09a13.584,13.584,0,0,1,13.121-12.807v2.86h.732V144.9a13.587,13.587,0,0,1,13.134,12.808h-3.338v.73h3.367v-.116c0,.081.013.159.013.242A13.6,13.6,0,0,1,315.943,172.231Z" />
-                        <path id="Path_226" data-name="Path 226"
-                            d="M316.1,152.925l-.037-.005v-2.839c0-.172-.217-.31-.485-.31s-.485.138-.485.31v2.839l-.038.005a5.375,5.375,0,1,0,1.045,0Zm-.524,10a4.629,4.629,0,0,1-.87-9.169V156a2.367,2.367,0,0,0-1.65,2.22,2.527,2.527,0,0,0,5.044,0,2.368,2.368,0,0,0-1.65-2.22v-2.247a4.629,4.629,0,0,1-.874,9.169Z" />
-                    </g>
+                <svg fill="currentColor" class="size-7" viewBox="0 0 50 50" xmlns="http://www.w3.org/2000/svg"
+                    xmlns:xlink="http://www.w3.org/1999/xlink">
+                    <path
+                        d="M17 2C16.449219 2 16 2.449219 16 3L16 11C16 11.550781 16.449219 12 17 12L33 12C33.554688 12 34 11.550781 34 11L34 3C34 2.449219 33.554688 2 33 2 Z M 29 7L31 7L31 9L29 9 Z M 22 14L22 30L10 30C9.675781 30 9.375 30.167969 9.1875 30.4375L2.46875 40L47.53125 40L40.8125 30.4375C40.625 30.167969 40.324219 30 40 30L28 30L28 14 Z M 2 42L2 45C2 45.554688 2.449219 46 3 46L4 46L4 47L6 47L6 46L44 46L44 47L46 47L46 46L47 46C47.554688 46 48 45.554688 48 45L48 42Z" />
                 </svg>
                 <div class="relative px-2 pt-2">
                     <span class="absolute top-0 left-2 text-xs w-40">@lang('messages.warehouse')</span>
@@ -53,8 +48,7 @@
                                     <p class="text-gray-600 font-bold text-sm">@lang('messages.deleteitemwarning').</p>
                                     <p class="text-gray-600 text-sm mb-5">@lang('messages.deleteitemconfirm')?</p>
                                     <div class="flex flex-col md:flex-row gap-2 justify-between">
-                                        <x-primary-button type="submit"
-                                            class="bg-red-600 hover:bg-red-700">
+                                        <x-primary-button type="submit" class="bg-red-600 hover:bg-red-700">
                                             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
                                                 stroke-width="1.5" stroke="currentColor" class="size-5">
                                                 <path stroke-linecap="round" stroke-linejoin="round"
@@ -77,8 +71,7 @@
                     </form>
                 </div>
 
-                <div
-                    class="w-full shadow-lg bg-primary-50 rounded-md border border-primary-100">
+                <div class="w-full shadow-lg bg-primary-50 rounded-md border border-primary-100">
                     <div class="p-4 space-y-2">
 
                         <div class="flex flex-col lg:flex-row">
